3df270e80bef0aedd7bc907d3de245a9801b5cb4
mehbubheroku <mehbubbasha557@gmail.com>
2020-09-27 07:53:40 +0530

    make it better code

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import flask
 from github import Github
 from flask import Flask, request, render_template, jsonify, send_file
-from jinja2 import Template
 import requests
 import pandas as pd
 import datetime
@@ -9,35 +8,39 @@ from datetime import timedelta
 import dateutil.parser
 import json
 
-# or using an access token
+# using an access token
 g = Github("93e27cd53dd1f8f4ba1ce9b120dfa62f8892fa3b")
 mytoken = "93e27cd53dd1f8f4ba1ce9b120dfa62f8892fa3b"
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
+#download git hub analysis file of my repo
 @app.route('/public_repo_download', methods=['GET'])
 def public_repo_download():
     txtfile = 'githubanalysis.txt'
     print("Saved commits information to commits_info.csv")
     return send_file(txtfile, as_attachment=True, cache_timeout=0)
 
-
+#download git hub analysis file of public repo
 @app.route('/my_repo_download', methods=['GET'])
 def my_repo_download():
     txtfile = 'githubanalysis.txt'
     print("Saved commits information to commits_info.csv")
     return send_file(txtfile, as_attachment=True, cache_timeout=0)
 
+#Rendering Home page
 @app.route('/')
 def getmetric():
     return render_template('metric.html')
 
+#fetch public get repo details
 @app.route('/public_metric')
 def public_metric():
     return render_template('public_metric.html')
 
+
+#fetch my repo details using GITHUB API requests
 @app.route('/get_pull_info')
 def getpullinfo():
     query_url = "https://api.github.com/repos/projetcongo/demoapp/"
@@ -49,14 +52,17 @@ def getpullinfo():
     issue_close = requests.get(query_url+"pulls?state=close", headers=headers, data={})
     data = issue_close.json()
 
+#using pandas am creating dataframe
     df = pd.DataFrame(data)
-    # print(df)
     df['closed_at'] = df['closed_at'].map(dateutil.parser.parse)
     df['created_at'] = df['created_at'].map(dateutil.parser.parse)
     df['tdiff'] = df['closed_at'] - df['created_at']
-    # print(df)
     use_cols = ['created_at', 'closed_at', 'tdiff']
+
+    #calculating mena value
     mean = str(df['tdiff'].mean())
+
+    #writing data into text file
     with open('githubanalysis.txt', 'w') as outfile:
         outfile.write('@ Repo details: \n')
 
@@ -81,20 +87,14 @@ def getpullinfo():
         outfile.write('\n')
         outfile.write('No of Users: '+ str(len(pulldata.json())))
 
-
-
-
-
-
-    # mydictionary.append(comitdata)
     data = []
     data.append({"pull_data": pulldata.json()})
     data.append({"comit_data": comitdata.json()})
     data.append({"issue_data": issue_data.json()})
     data.append({"metric": [{"avg_time": mean}]})
-    print(data)
     return json.dumps(data)
 
+#fetch public repo details using GITHUB API requests
 @app.route('/get_pull_info_public')
 def get_pull_info_public():
     query_url = "https://api.github.com/repos/github/platform-samples/"
@@ -107,11 +107,9 @@ def get_pull_info_public():
     data = issue_close.json()
 
     df = pd.DataFrame(data)
-    # print(df)
     df['closed_at'] = df['closed_at'].map(dateutil.parser.parse)
     df['created_at'] = df['created_at'].map(dateutil.parser.parse)
     df['tdiff'] = df['closed_at'] - df['created_at']
-    # print(df)
     use_cols = ['created_at', 'closed_at', 'tdiff']
     mean = str(df['tdiff'].mean())
     with open('publicgithubanalysis.txt', 'w') as outfile:
@@ -138,7 +136,6 @@ def get_pull_info_public():
         outfile.write('\n')
         outfile.write('No of Users: '+ str(len(pulldata.json())))
 
-    # mydictionary.append(comitdata)
     data = []
     data.append({"pull_data": pulldata.json()})
     data.append({"comit_data": comitdata.json()})
